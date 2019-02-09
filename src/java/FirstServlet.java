158937f7b244e4cfbc32522bfcf0a3dab0db7372
NafanyaRussian <31651913+NafanyaRussian@users.noreply.github.com>
2019-02-09 18:58:33 +0400

    test

---
+++ src/java/FirstServlet.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+  
+@WebServlet("/hello")
+public class FirstServlet extends HttpServlet {
+  
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        
+        String[] names = new String[]{"Oleg", "Vlad", "Sam"};
+        request.setAttribute("names", names);
+        
+        getServletContext()
+            .getRequestDispatcher("/hello.jsp")
+            .forward(request, response);
+    }
+    
+}
